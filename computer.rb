b12c998f6cf45e10af258925d28a4eb161f0165f
Sarah <sarahrosedahnke@gmail.com>
2017-10-25 00:20:08 +0000

    update2

--- computer.rb
+++ computer.rb
@@ -1,6 +1,5 @@
-class Computer
+module Computer
 require_relative "board"
-attr_accessor 
 
 def optimal(player_history, computer_history)
 	win_array=[[0,1,2], [0,4,8], [0,3,6], [1,4,7], [2,5,8], [2,4,6], [3,4,5], [6,7,8]]
